2e9b0be773c5c0435ff9c9cd7f53d1b8b58788b0
jack <liaojk@gz2000.net>
2021-05-29 17:18:41 +0800

    修改服务2端口 服务名称

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,20 +1,34 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@Configurable
 @RequestMapping("/test")
 public class TestController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @ResponseBody
     @RequestMapping("/t1")
     public Map t1(){
+        Map ss=new HashMap();
+        ss.put("v1",new Date());
+        ss.put("v2","嚯嚯嚯我是服务1");
+        return  ss;
+    }
+
+    public Map t2(){
         Map ss=new HashMap();
         ss.put("v1",new Date());
         ss.put("v2","嚯嚯嚯");
