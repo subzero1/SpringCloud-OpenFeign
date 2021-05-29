512a343ce7e865914354ba6cbbd1c94192fdd8ba
jack <liaojk@gz2000.net>
2021-05-29 19:21:03 +0800

    服务间调用RestTemplate bug修复

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -2,6 +2,9 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,26 +15,30 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configurable
+@RestController
+@Configuration
 @RequestMapping("/test")
 public class TestController {
 
     @Autowired
-    RestTemplate restTemplate;
+    private  RestTemplate restTemplate;
 
-    @ResponseBody
     @RequestMapping("/t1")
     public Map t1(){
         Map ss=new HashMap();
         ss.put("v1",new Date());
-        ss.put("v2","嚯嚯嚯我是服务1");
+        ss.put("v2","嚯嚯嚯我是服务2");
         return  ss;
     }
-
+    @RequestMapping("/t2")
     public Map t2(){
-        Map ss=new HashMap();
-        ss.put("v1",new Date());
-        ss.put("v2","嚯嚯嚯");
-        return  ss;
+        Map body = restTemplate.getForObject("http://EUREKA-CLIENT1/test/t1", Map.class);
+        body.put("k2","TMD终于调用成功了，真tm不容易");
+        return  body;
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return  new RestTemplate();
     }
 }
